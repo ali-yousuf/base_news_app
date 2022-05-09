47c779cea3e6c331a9460515d350d5c013f1e7d2
yousufaits22 <yousufaits22@gmail.com>
2022-05-09 15:46:15 +0600

    set up theme cubit

--- lib/core/base_news_app.dart
+++ lib/core/base_news_app.dart
@@ -1,19 +1,27 @@
 import 'package:base_news_app/core/constants.dart';
+import 'package:base_news_app/core/themes/app_themes.dart';
+import 'package:base_news_app/core/themes/cubit/theme_cubit.dart';
 import 'package:base_news_app/navigation_bar/view/navigation_bar_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class BaseNewsApp extends StatelessWidget {
   const BaseNewsApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: Constants.appName,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return BlocProvider(
+      create: (context) => ThemeCubit(),
+      child: BlocBuilder<ThemeCubit, bool>(
+        builder: (context, state) {
+          return MaterialApp(
+            title: Constants.appName,
+            debugShowCheckedModeBanner: false,
+            theme: state ? AppThemes.darkTheme : AppThemes.lightTheme,
+            home: const NavigationBarPage(),
+          );
+        },
       ),
-      home: const NavigationBarPage(),
     );
   }
 }
---
+++ lib/core/themes/app_themes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppThemes {
+  static final ThemeData lightTheme = ThemeData().copyWith();
+  static final ThemeData darkTheme = ThemeData().copyWith();
+}
